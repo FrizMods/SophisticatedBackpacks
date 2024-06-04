43b4475e395e61db73540e5f0112dac4bb00cc49
frizzle101101 <aaronfritz96@gmail.com>
2024-06-04 03:39:54 -0400

    cherry pick f6b3f37 and fix for 1.20.1 minecraft

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/SBPButtonDefinitions.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/SBPButtonDefinitions.java
@@ -6,6 +6,7 @@ import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Dimension;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Position;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.UV;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.ContentsFilterType;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.crafting.CraftingRefillType;
 
 import java.util.Map;
 
@@ -27,5 +28,14 @@ public class SBPButtonDefinitions {
 							SBPTranslationHelper.INSTANCE.getTranslatedLines(SBPTranslationHelper.INSTANCE.translUpgradeButton("shift_click_into_backpack"), null)),
 					false, getButtonStateData(new UV(48, 48), Dimension.SQUARE_16, new Position(1, 1),
 							SBPTranslationHelper.INSTANCE.getTranslatedLines(SBPTranslationHelper.INSTANCE.translUpgradeButton("shift_click_into_inventory")))
+			));
+
+	public static final ButtonDefinition.Toggle<CraftingRefillType> REFILL_CRAFTING_GRID = net.p3pp3rf1y.sophisticatedcore.client.gui.controls.ButtonDefinitions.createToggleButtonDefinition(
+			Map.of(
+					CraftingRefillType.DisableRefill, getButtonStateData(new UV(96, 80), SBPTranslationHelper.INSTANCE.translUpgradeButton("disable_refill_crafting_grid"), Dimension.SQUARE_16, new Position(1, 1)),
+					CraftingRefillType.RefillFromStorage, getButtonStateData(new UV(112, 80), SBPTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid_from_storage"), Dimension.SQUARE_16, new Position(1, 1)),
+					CraftingRefillType.RefillFromPlayer, getButtonStateData(new UV(128, 80), SBPTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid_from_player"), Dimension.SQUARE_16, new Position(1, 1)),
+					CraftingRefillType.RefillFromPlayerThenStorage, getButtonStateData(new UV(144, 80), SBPTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid_from_player_then_storage"), Dimension.SQUARE_16, new Position(1, 1)),
+					CraftingRefillType.RefillFromStorageThenPlayer, getButtonStateData(new UV(160, 80), SBPTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid_from_storage_then_player"), Dimension.SQUARE_16, new Position(1, 1))
 			));
 }
